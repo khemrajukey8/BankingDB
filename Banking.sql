4378b3c77dddea5354cdb0dbf32dd6263534ccac
khemrajukey8 <khemrajukey8@gmail.com>
2026-07-29 09:15:58 +0530

    Create More Table

--- Banking.sql
+++ Banking.sql
@@ -14,3 +14,48 @@
  
  select* from Customers;
  
+ Create table accounts (
+ AccountID INT,
+ AccountType varchar(50),
+ Balance DECIMAL (10,2)
+ );
+ desc accounts;
+ 
+ create table Trasaction (
+ TrasactionID INT,
+ TrasactionDate DATE,
+ Amount DECIMAL (10,2),
+ TrasactionType varchar(20)
+ );
+ 
+ desc Trasaction;
+ ALTER TABLE Trasaction
+RENAME TO Transaction;
+
+create table Branches (
+BranchID INT ,
+BranchName varchar(100),
+BranchAddress varchar(200),
+BranchPhone varchar(15)
+);
+
+ desc Branches;
+ 
+ create table Acccountbranches (
+ AssignmentDate DATE 
+ );
+ 
+ CREATE TABLE Loans (
+ LoanID INT,
+ LoanAmount decimal(10,2),
+ InterestRate decimal(5,2),
+ StartDate DATE ,
+ EndDate DATE 
+ );
+ desc Loans;
+ 
+ 
+ 
+ 
+ 
+ 
