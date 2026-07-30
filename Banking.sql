c3f38105eb85d6e4254a824df4b0bc899b4c367a
khemrajukey8 <khemrajukey8@gmail.com>
2026-07-30 09:03:57 +0530

    using alter command

--- Banking.sql
+++ Banking.sql
@@ -53,7 +53,57 @@ BranchPhone varchar(15)
  EndDate DATE 
  );
  desc Loans;
+ alter table customers
+ add DateOfBirth DATE;
+ desc customers;
+ alter table customers modify Phone varchar(20);
  
+ alter table accounts
+ add constraint chk_MinBalance 
+ check (Balance >=1000);
+ 
+ drop table acccountbranches; 
+ 
+ alter table accounts
+ add CustomerID INT;
+ 
+ alter table accounts
+ add constraint  FK_Accounts_Customers
+ foreign key (customerID)
+ references customers(customerID);
+ desc accounts;
+ 
+ alter table accounts
+ add constraint primary key (AccountID);
+ desc accounts;
+ 
+ alter table customers
+ modify FirstName varchar(50) NOT NULL ;
+ desc customers;
+
+alter table customers
+ADD constraint uq_email 
+unique (email);
+
+alter table branches
+add constraint pk_branchID
+primary key(BranchID);
+desc branches;
+
+alter table accounts
+add BranchID INT;
+
+ alter table accounts
+ add constraint fk_accounts_branch
+foreign key (BranchID)
+references Branches(BranchID);
+
+desc accounts;
+
+
+ 
+
+
  
  
  
