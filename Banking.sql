ee01498ff7fccd0f8d6588003afffe56aba76c7b
khemrajukey8 <khemrajukey8@gmail.com>
2026-08-05 22:00:23 +0530

    filter using DQL Command

--- Banking.sql
+++ Banking.sql
@@ -160,6 +160,119 @@ values (103, 'Nadira' , 'Ukey' , 'nadiraukey20@gmail.com', '9373248950', '2001-1
 (104, 'Komal' , 'Tekam' , 'komaltakem@gmail.com', '8408819296', '2015-04-15','2003-03-14');
 select*from customers;
 
+INSERT INTO branches
+(BranchID, BranchName, BranchAddress, BranchPhone)
+VALUES
+(1, 'Main Branch', 'Near Ganesh Bus Stand, Nagpur', '9565458237'),
+(2, 'Civil Lines Branch', 'Civil Lines, Nagpur', '9373248950'),
+(3, 'Dharampeth Branch', ' Dharampeth, Nagpur', '7122233447');
+
+ 
+  INSERT INTO accounts
+ (AccountID, AccountType, Balance, CustomerID, BranchID)
+ VALUES
+ (202, 'Current', 50000, 102, 2),
+ (203, 'Saving', 15000, 103, 3);
+ 
+ UPDATE accounts
+SET BranchID = 1
+WHERE AccountID = 201;
+
+INSERT INTO loans
+(LoanID, LoanAmount, InterestRate, StartDate, EndDate)
+VALUES
+(3001, 1500000.00, 10.12, '2026-01-15', '2046-01-15'),
+
+(3002, 500000.00, 9.25, '2026-02-10', '2031-02-10'),
+
+(3003, 300000.00, 7.80, '2026-05-05', '2030-05-05'),
+
+(3004, 200000.00, 11.50, '2026-01-20', '2029-01-20');
+
+update loans
+set CustomerID = '101'
+where loanID = 3001;
+
+update loans
+set CustomerID = '102'
+where loanID = 3002;
+
+update loans
+set CustomerID = '103'
+where loanID = 3003;
+
+DELETE FROM loans
+WHERE LoanID = 3004;
+
+insert into transaction (TrasactionID, TrasactionDate, Amount , TrasactionType , AccountID)
+values ( 401, '2026-05-18', '10000.10' , 'Deposit' , 201),
+( 402, '2026-06-20' , '20000.00', 'Withdrawal' , 202),
+( 403, '2026-06-25' , '30000.00' , 'Deposit' , 203);
+
+UPDATE accounts 
+SET 
+    balance = balance + 5000
+WHERE
+    accountID = 202;
+
+update accounts
+set balance = '30000'
+where accountID = 201;
+
+UPDATE customers 
+SET 
+    email = 'khemraj.ukey7@gmail.com',
+    phone = 9373248950
+WHERE
+    customerID = 101;
+    
+SELECT 
+    customerID,FirstName, LastName, Phone
+FROM
+    customers;
+    
+SELECT 
+    *
+FROM
+    accounts
+WHERE
+    AccountType = 'Saving';
+    
+SELECT 
+    *
+FROM
+    accounts
+WHERE
+    balance <=30000 ;
+    
+insert into branches
+values (4, 'sadar Branch', 'Medical Square ' , 8408819296);
+ insert into accounts
+ values (204, 'Current' ,25000, 104, 4);
+ 
+ select * from accounts 
+ where AccountType != 'saving';
+ 
+ select * from accounts 
+ where not AccountType = 'saving';
+ 
+ select * from accounts
+ where Balance >=30000 and AccountType = 'Saving' ; 
+ 
+ select * from accounts
+ where Balance >=30000 && AccountType = 'Saving' ; 
+ 
+ 
+
+SELECT * FROM branches;
+select * from accounts;
+select * from loans;
+select * from customers;
+select * from transaction;
+
+
+
+ 
 
 
  
